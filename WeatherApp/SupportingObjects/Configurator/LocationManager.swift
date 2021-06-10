438a4fb464fe0f42ca097a09718ed35519762a5e
Rodrigo Torres <rtorres@intermediait.com>
2021-06-10 16:23:16 -0300

    Added Permisions for Location Services

---
+++ WeatherApp/SupportingObjects/Configurator/LocationManager.swift
@@ -0,0 +1,18 @@
+//
+//  LocationConfigurator.swift
+//  WeatherApp
+//
+//  Created by Rodrigo Torres on 09/06/2021.
+//
+import Foundation
+import CoreLocation
+
+
+class LocationManager: NSObject  {
+    
+    
+    
+ 
+    
+    
+}
